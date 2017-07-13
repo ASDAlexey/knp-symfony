e3e07078c775f5009d2493e4f6312a8d02530fda
Alexey Popov <alexey.popov@umbrella-web.com>
2017-07-14 00:13:40 +0300

    created new genus route and bind with DB

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -2,12 +2,27 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller {
+    /**
+     * @Route("/genus/new")
+     */
+    public function newAction() {
+        $genus = new Genus();
+        $genus->setName('Octupus' . rand(1, 100));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response('Genus created!');
+    }
+
     /**
      * @Route("/genus/{genusName}")
      */
