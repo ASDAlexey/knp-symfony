22c0ced6ba6b81d86de616da1cd75ce53b1722dc
Alexey Popov <alexey.popov@umbrella-web.com>
2017-07-28 16:51:11 +0300

    added isPublished

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -38,6 +38,11 @@ class Genus {
     private $funFact;
 
     /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
+
+    /**
      * @return mixed
      */
     public function getName() {
@@ -95,5 +100,12 @@ class Genus {
 
     public function getUpdatedAt() {
         return new \DateTime('-' . rand(0, 100) . ' days');
+    }
+
+    /**
+     * @param mixed $isPublished
+     */
+    public function setIsPublished($isPublished) {
+        $this->isPublished = $isPublished;
     }
 }
