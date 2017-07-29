ac5e91963ffc29bd4b747036351967e22617cbb1
Alexey Popov <alexey.popov@umbrella-web.com>
2017-07-29 18:19:33 +0300

    added bootstrap from type

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -22,6 +23,8 @@ class GenusAdminController extends Controller {
      * @Route("/genus/new", name="admin_genus_new")
      */
     public function newAction() {
-        // let's go to work!
+        $form = $this->createForm(GenusFormType::class);
+
+        return $this->render('admin/genus/new.html.twig', ['genusForm' => $form->createView()]);
     }
 }
