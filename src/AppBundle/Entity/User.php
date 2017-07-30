d7ea3c07aadc0201b9372b9566e24ec2e9b077d4
Alexey Popov <alexey.popov@umbrella-web.com>
2017-07-30 19:52:20 +0300

    add uniq email

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,12 +3,15 @@
 namespace AppBundle\Entity;
 
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="user")
+ * @UniqueEntity(fields={"email"}, message="Email already in use")
  */
 class User implements UserInterface {
     /**
@@ -19,6 +22,8 @@ class User implements UserInterface {
     private $id;
 
     /**
+     * @Assert\NotBlank()
+     * @Assert\Email()
      * @ORM\Column(type="string", unique=true)
      */
     private $email;
@@ -28,6 +33,9 @@ class User implements UserInterface {
      */
     private $password;
 
+    /**
+     * @Assert\NotBlank()
+     */
     private $plainPassword;
 
     /**
